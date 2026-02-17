551ec5362b8050e2da0ab238b1ce59e43222ee0e
Mykhailo Tiupenko <mykhailt@stud.ntnu.no>
2026-02-17 12:21:33 +0100

    Update CapitalizeWordsTextCommand.java
    documentation

--- src/main/java/org/example/CapitalizeWordsTextCommand.java
+++ src/main/java/org/example/CapitalizeWordsTextCommand.java
@@ -3,6 +3,12 @@ package org.example;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+/**
+ * A {@link TextCommand} which capitalizes the first letter of each word in the string
+ * hild of CapitalizeTextCommand
+ * limitations. everything except the first one
+ * it is applied to. No more than one whitespace
+ */
 public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
 
     @Override
@@ -10,11 +16,15 @@ public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
         if (text == null || text.isEmpty()) {
             return text;
         }
-
-        return Arrays.stream(text.split(" "))
+        String collect = Arrays.stream(text.split("\n"))
                 .map(word -> word.isEmpty()
                         ? word
                         : Character.toUpperCase(word.charAt(0)) + word.substring(1))
+                .collect(Collectors.joining("\n"));
+        return Arrays.stream(collect.split(" "))
+                .map(word -> word.isEmpty()
+                        ? word
+                        : super.execute(word))
                 .collect(Collectors.joining(" "));
     }
 }
