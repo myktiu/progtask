fd7572b37fdf55160be0bade986d0a59bb1dc576
Mykhailo Tiupenko <mykhailt@stud.ntnu.no>
2026-02-17 12:22:11 +0100

    Update Main.java
    example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,32 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+/**
+ Limitations Capitalize need to be executed before wrapping
+ */
 public class Main {
     public static void main(String[] args) {
+        String text = """
+                hello world
+                this is a selection test
+                another world appears here
+                final selection line
+                """;
 
+        Script script = new Script(List.of(
+                new ReplaceTextCommand("world", "java"),
+                new ReplaceFirstTextCommand("selection", "TARGET"),
+                new CapitalizeTextCommand(),
+                new CapitalizeWordsTextCommand(),
+                new CapitalizeSelectionTextCommand("java"),
+                new WrapLinesTextCommand("[", "]"),
+                new WrapSelectionTextCommand("<b>", "</b>", "TARGET"),
+                new WrapTextCommand("<<<START>>>\n", "\n<<<END>>>")
+        ));
 
+        String result = script.execute(text);
+
+        System.out.println("FINAL RESULT:\n");
+        System.out.println(result);
     }
 }
